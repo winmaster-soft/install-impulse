86be20a3de2ccb119dd1b7003e31355a11a9c595
winmaster-soft <69120348+winmaster-soft@users.noreply.github.com>
2020-11-02 22:16:32 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+man sleep
+
+echo Скачивание необходимых компонентов...
+echo
+git clone https://github.com/LimerBoy/Impulse
+clear
+echo Настройка...
+sleep 5s
+mv start ../Impulse/
+clear
+echo Удаление лишних файлов...
+sleep 5s
+cd
+rm -rf installer
+cd Impulse
+clear
+echo Установка зависииостей...
+echo
+pip install -r requirements.txt
+clear
+echo Запуск...
+sleep 5s
+. start
