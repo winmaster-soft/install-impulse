ff503b4f7ffe7633f7879cc3d9a9ce1ed7aebe0a
winmaster-soft <69120348+winmaster-soft@users.noreply.github.com>
2020-11-02 22:42:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo Скачивание необходимых компонентов...;echo;git clone https://github.com/LimerBoy/Impulse;clear;echo Настройка...;sleep 5s;mv start ../Impulse/;clear;echo Удаление лишних файлов...;sleep 5s;cd;rm -rf installer;cd Impulse;clear;echo Установка зависииостей...;echo;pip install -r requirements.txt;clear;echo Запуск...;sleep 5s;. start
+cd ../;clear;echo Скачивание необходимых компонентов...;echo;git clone https://github.com/LimerBoy/Impulse;cd ../install;clear;echo Настройка...;sleep 5s;mv start ../Impulse/;clear;echo Удаление лишних файлов...;sleep 5s;cd;rm -rf installer;cd Impulse;clear;echo Установка зависииостей...;echo;pip install -r requirements.txt;clear;echo Запуск...;sleep 5s;. start
